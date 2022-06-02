3a4e717906f1e19443b414133652726b292863cf
VovaHash <vovahash01@gmail.com>
2022-06-02 23:49:01 +0300

    update card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header"
 import PhotoGrid from "./components/PhotoGrid"
+import Card from './components/Card'
 
 function App() {
   return (
     <>
    <Header/>
    <PhotoGrid/>
+   <Card/>
    </>
   );
 }
