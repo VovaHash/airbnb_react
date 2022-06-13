a4348963db8a89655a1ed97eff47939af9160cf7
VovaHash <vovahash01@gmail.com>
2022-06-14 00:21:57 +0300

    badge and object as a prop

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,20 +3,20 @@
 export default function Card(props) {
   
   let badgeText;
-  if (props.openSpots === 0) {
+  if (props.item.openSpots === 0) {
     badgeText = "SOLD OUT"
-  } else if (props.location == "Online"){
+  } else if (props.item.location == "Online"){
     badgeText = "ONLINE"
   }
-  
+
   return (
    
     <div className="card">
-        { props.openSpots === 0 && <div className="card--badge">{badgeText}</div> }
-        <img className="card--image" src={`./images/${props.img}`} />
-        <h5 className="card--text">{'\u2B50'} {props.rating} <span className="grey">({props.reviewCount}) • {props.location}</span></h5>
-        <h5 className="card--text">{props.title}</h5>
-        <h5 className="card--text"><b>From ${props.price}</b>/ person</h5>
+        { badgeText && <div className="card--badge">{badgeText}</div> }
+        <img className="card--image" src={`./images/${props.item.coverImg}`} />
+        <h5 className="card--text">{'\u2B50'} {props.item.stats.rating} <span className="grey">({props.item.stats.reviewCount}) • {props.item.location}</span></h5>
+        <h5 className="card--text">{props.item.title}</h5>
+        <h5 className="card--text"><b>From ${props.item.price}</b>/ person</h5>
     </div>
   );
 }
